0e28de6417b0f9713bd44acdfcbd9ab5537d18a6
Bartlomiej Lechowski <bartlomiej.lechowski@rst-it.com>
2018-12-04 21:20:03 +0100

    AB-43 findOneAndUpdate

--- examples/mongoose/admin-model.js
+++ examples/mongoose/admin-model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const SALT_WORK_FACTOR = 10
 const { Schema } = mongoose
+const SALT_WORK_FACTOR = 10
 
 const AdminSchema = new Schema({
   email: String,
@@ -9,48 +9,22 @@ const AdminSchema = new Schema({
 })
 
 AdminSchema.pre('save', function(next) {
-  console.log('saave')
   const admin = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!admin.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-      if (err) return next(err);
-
-      // hash the password using our new salt
-      bcrypt.hash(admin.password, salt, (err, hash) => {
-          if (err) return next(err);
-
-          // override the cleartext password with the hashed one
-          admin.password = hash;
-          next();
-      });
+  bcrypt.hash(admin.password, SALT_WORK_FACTOR, (err, hash) => {
+    if (err) return next(err);
+    admin.password = hash;
+    next();
   });
 });
 
-AdminSchema.pre('update', function(next) {
-  console.log('duupa', this.password)
-  const admin = this;
-
-  // only hash the password if it has been modified (or is new)
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-      if (err) return next(err);
-
-      // hash the password using our new salt
-      bcrypt.hash(admin.password, salt, (err, hash) => {
-          if (err) return next(err);
-
-          // override the cleartext password with the hashed one
-          admin.password = hash;
-          next();
-      });
+AdminSchema.pre('findOneAndUpdate', function(next) {
+  const update = this._update.$set
+  bcrypt.hash(update.password, SALT_WORK_FACTOR, (err, hash) => {
+    if (err) return next(err);
+    update.password = hash;
+    next();
   });
-});
-
+})
 
 const Admin = mongoose.model('Admin', AdminSchema)
 
