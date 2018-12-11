0395830b37e808b7805bf3ddee84a4c97e87ca8b
Michal Laskowski <michlask@gmail.com>
2018-12-11 12:23:57 +0100

    AB-55 Add datepicker and momentjs

--- src/backend/utils/base-decorator.js
+++ src/backend/utils/base-decorator.js
@@ -1,5 +1,6 @@
 const BaseProperty = require('../adapters/base-property')
 const ViewHelpers = require('./view-helpers')
+const moment = require('moment')
 
 const DEFAULT_MAX_ITEMS_IN_LIST = 5
 
@@ -203,6 +204,9 @@ class BaseDecorator {
    * @return {String}                        Html string which will be rendered
    */
   getValue({ record, property, where }) {
+    if (property.type() === 'date') {
+      return moment(record.param(property.name())).format("D MMM YYYY")
+    }
     return record.param(property.name())
   }
 }
--- src/frontend/scripts/app.js
+++ src/frontend/scripts/app.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
   $('.hamburger').click(() => $('.sidebar-desktop').removeClass('sidebar-mobile'));
   $('.content-wrapper').click(() => $('.sidebar-desktop').addClass('sidebar-mobile'));
+
+  if ($('.pickadate')) {
+    $('.pickadate').pickadate({
+      format: 'd mmm yyyyy',
+      firstDay: 1,
+    })
+  }
 });
