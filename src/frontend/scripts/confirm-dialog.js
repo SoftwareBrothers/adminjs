3b73dd20799841ec67004488ed7df78e7b8a1383
Bartłomiej Lechowski <frontkick.it@gmail.com>
2019-01-07 11:58:00 +0100

    AB-77 travis linter

--- src/frontend/scripts/confirm-dialog.js
+++ src/frontend/scripts/confirm-dialog.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
 $(document).ready(() => {
   const $confirmDialog = $('.confirm-dialog')
   $('[data-confirm-dialog]').click(event => showDialog(event))
   $('.confirm-cancel').click(() => $confirmDialog.removeClass('active'))
 
-  const showDialog = event => {
+  const showDialog = (event) => {
     const path = $(event.currentTarget).data('confirm-dialog')
     $confirmDialog.addClass('active')
-    $('.confirm-accept').attr('href', path);
+    $('.confirm-accept').attr('href', path)
   }
 })
