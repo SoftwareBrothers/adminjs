b131c60b9eb33edc101d77bf1595eeedd58cb60f
Michal Laskowski <michlask@gmail.com>
2019-01-07 16:06:52 +0100

    AB-80 Toggle in sidebar, mobile hamburger and manu animation, dropdown menu opacity

--- src/frontend/scripts/app.js
+++ src/frontend/scripts/app.js
@@ -1,12 +1,34 @@
 $(document).ready(() => {
-  $('.hamburger').click(() => $('.sidebar-desktop').removeClass('sidebar-mobile'));
-  $('.content-wrapper').click(() => $('.sidebar-desktop').addClass('sidebar-mobile'));
+  const $sidebar = $('.sidebar')
+  $('.hamburger').click(() => $sidebar.toggleClass('sidebar-show'))
+  $('.content-wrapper section').click(() => $sidebar.removeClass('sidebar-show'))
 
-  const $pickadate = $('.pickadate');
+  const $pickadate = $('.pickadate')
   if ($pickadate) {
     $pickadate.pickadate({
       format: 'yyyy-mm-dd',
       firstDay: 1,
     })
-  };
-});
+  }
+
+  const $dropdownToggle = $('.dropdown-toggle')
+  const toggleMenu = ($element) => {
+    $element.parent().siblings('.dropdown-list').slideToggle()
+    $element.toggleClass('icomoon-dropdown-open').toggleClass('icomoon-dropdown-close')
+  }
+  if ($dropdownToggle) {
+    $dropdownToggle.on('click', (event) => {
+      const $target = $(event.target)
+      toggleMenu($target)
+      const status = $target.hasClass('icomoon-dropdown-open') ? 'open' : 'close'
+      localStorage.setItem($target.data('menu'), status)
+    });
+
+    $dropdownToggle.each((index, element) => {
+      const $element = $(element)
+      if (localStorage.getItem($element.data('menu')) === 'close') {
+        toggleMenu($element)
+      }
+    })
+  }
+})
