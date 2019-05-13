0001e506a73ba8c4e07cf486268ef4a896aef404
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-05-13 12:55:09 +0200

    bring back old action per resource

--- src/frontend/components/routes/record-action.jsx
+++ src/frontend/components/routes/record-action.jsx
@@ -31,7 +31,7 @@ class RecordAction extends React.Component {
       || newProps.match.params.recordId !== recordId
       || newProps.match.params.resourceId !== resourceId
     ) {
-      this.fetchRecord()
+      this.fetchRecord(newProps.match.params.actionName)
       return false
     }
     return true
@@ -48,21 +48,22 @@ class RecordAction extends React.Component {
     return { resource, action }
   }
 
-  fetchRecord() {
+  fetchRecord(newActionName) {
     const { match } = this.props
-    const { recordId, resourceId } = match.params
+    const { recordId, resourceId, actionName } = match.params
     const api = new ApiClient()
     this.setState({
       isLoading: true,
       record: null,
     })
-    api.getRecord({
+    api.recordAction({
       resourceId,
       recordId,
-    }).then((record) => {
+      actionName: newActionName || actionName,
+    }).then((response) => {
       this.setState({
         isLoading: false,
-        record,
+        record: response.data.record,
       })
     })
   }
