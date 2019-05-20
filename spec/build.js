063cd20e100511268154df9d8de2874e9fae133a
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-05-20 21:51:30 +0200

    fix tests on node v8

--- spec/build.js
+++ spec/build.js
@@ -1,21 +1,5 @@
 /* eslint-disable func-names */
-process.env.NODE_ENV = 'test'
-
-const chai = require('chai')
-const sinonChai = require('sinon-chai')
-const sinon = require('sinon')
-
-chai.use(sinonChai)
-
-global.expect = chai.expect
-
-beforeEach(function () {
-  this.sinon = sinon.createSandbox()
-})
-
-afterEach(function () {
-  this.sinon.restore()
-})
+require('./setup')
 
 require('require.all')({
   dir: '../build/',
