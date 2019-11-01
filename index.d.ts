a175319ac3efad24dde05b542c833aea1c95b043
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-11-01 14:13:16 +0100

    add json types

--- index.d.ts
+++ index.d.ts
@@ -1,48 +1,36 @@
 import AdminBro from './types/src/admin-bro'
-import AdminBroOptions from './types/src/admin-bro-options.interface'
-import { ResourceOptions } from './types/src/backend/decorators/resource-options.interface'
-import PropertyOptions from './types/src/backend/decorators/property-options.interface'
-import Action from './types/src/backend/actions/action.interface'
-import { CurrentAdmin } from './types/src/current-admin.interface'
 
-import BaseProperty from './types/src/backend/adapters/base-property'
-import BaseResource from './types/src/backend/adapters/base-resource'
-import BaseDatabase from './types/src/backend/adapters/base-database'
-import BaseRecord from './types/src/backend/adapters/base-record'
-import Router from './types/src/backend/router'
-import Filter from './types/src/backend/utils/filter'
-import ValidationError from './types/src/backend/utils/validation-error'
-import ApiClient from './types/src/frontend/utils/api-client'
 import * as ACTIONS from './types/src/backend/actions/index'
 import { ReduxState } from './types/src/frontend/store/store'
+
+export { default as Router } from './types/src/backend/router'
+export { default as Filter } from './types/src/backend/utils/filter'
+export { default as ValidationError } from './types/src/backend/utils/validation-error'
+export { default as ApiClient } from './types/src/frontend/utils/api-client'
+
+export { CurrentAdmin } from './types/src/current-admin.interface'
+export { ResourceOptions } from './types/src/backend/decorators/resource-options.interface'
+export { default as AdminBroOptions } from './types/src/admin-bro-options.interface'
+export { default as PropertyOptions } from './types/src/backend/decorators/property-options.interface'
+export { default as Action } from './types/src/backend/actions/action.interface'
+
+export { default as BaseProperty } from './types/src/backend/adapters/base-property'
+export { default as BaseResource } from './types/src/backend/adapters/base-resource'
+export { default as BaseDatabase } from './types/src/backend/adapters/base-database'
+export { default as BaseRecord } from './types/src/backend/adapters/base-record'
+
+export { default as RecordJSON } from './types/src/backend/decorators/record-json.interface'
+export { default as ResourceJSON } from './types/src/backend/decorators/resource-json.interface'
+export { default as ActionJSON } from './types/src/backend/decorators/resource-json.interface'
+export { default as PropertyJSON } from './types/src/backend/decorators/property-json.interface'
 
 export * from './types/src/frontend/components/ui'
 export * from './types/src/frontend/components/app'
 
 export {
   AdminBro as default,
-
-  // Base
-  BaseProperty,
-  BaseResource,
-  BaseDatabase,
-  BaseRecord,
-
-  // Classes
-  Router,
-  Filter,
-  ValidationError,
-  ApiClient,
-
   // Actions
   ACTIONS,
-
-  // Types
-  CurrentAdmin,
-  AdminBroOptions,
-  ResourceOptions,
-  PropertyOptions,
-  Action,
 }
 
 declare const REDUX_STATE: ReduxState
