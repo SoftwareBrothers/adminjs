1bc7a123b24afcdbcde55e5d3a0b5ab5980348d5
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-12-16 18:42:54 +0100

    styled input has default class which is input

--- src/frontend/components/ui/styled-input.tsx
+++ src/frontend/components/ui/styled-input.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-const StyledInput = styled.input`
+const StyledInput = styled.input.attrs(props => ({
+  className: props.className || 'input',
+}))`
   background: ${({ theme }): string => theme.colors.inputBck};
   color: ${({ theme }): string => theme.colors.defaultText};
   &:hover{
