671cf6a94fc270633fba119ce605a3f4e20e0c4e
Jakub Sapalski <jakub.sapalski@softwarebrothers.co>
2020-02-07 10:18:43 +0100

    Fix errors

--- src/frontend/components/actions/new.tsx
+++ src/frontend/components/actions/new.tsx
@@ -1,20 +1,22 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
 
-import PropertyType from "../property-type";
+import PropertyType from '../property-type'
 
-import withNotice, {
-  AddNoticeProps,
-} from "../../store/with-notice";
-import { ActionProps } from "./action.props";
-import { PropertyPlace } from "../../../backend/decorators/property-json.interface";
-import { DrawerContent, Box, DrawerFooter, Button } from "../design-system";
-import ActionHeader from "../app/action-header";
-import useResourceNew from "./utils/useResourceNew";
-import RecordJSON from "../../../backend/decorators/record-json.interface";
+import withNotice, { AddNoticeProps } from '../../store/with-notice'
+import { ActionProps } from './action.props'
+import { PropertyPlace } from '../../../backend/decorators/property-json.interface'
+import { DrawerContent, Box, DrawerFooter, Button } from '../design-system'
+import ActionHeader from '../app/action-header'
+import useResourceNew from './utils/useResourceNew'
+import RecordJSON from '../../../backend/decorators/record-json.interface'
 
-const New: FC<ActionProps & AddNoticeProps> = props => {
-  const {record: initialRecord, resource, addNotice, action} = props;
-  const {record, handleChange, handleSubmit} = useResourceNew(initialRecord!, resource.id, addNotice);
+const New: FC<ActionProps & AddNoticeProps> = (props) => {
+  const { record: initialRecord, resource, addNotice, action } = props
+  const { record, handleChange, handleSubmit } = useResourceNew(
+    initialRecord!,
+    resource.id,
+    addNotice,
+  )
 
   return (
     <Box
@@ -44,8 +46,7 @@ const New: FC<ActionProps & AddNoticeProps> = props => {
         </Button>
       </DrawerFooter>
     </Box>
-  );
-};
+  )
+}
 
-export default withNotice(New);
-
+export default withNotice(New)
--- src/frontend/components/actions/utils/useResourceNew.ts
+++ src/frontend/components/actions/utils/useResourceNew.ts
@@ -1,79 +1,86 @@
-import ApiClient from "../../../utils/api-client";
-import RecordJSON from "../../../../backend/decorators/record-json.interface";
-import { NoticeMessage } from "../../../store/with-notice";
-import { useState } from "react";
-import { useHistory } from "react-router";
-import recordToFormData from "../record-to-form-data";
-import { appendForceRefresh } from "./append-force-refresh";
+import { useState } from 'react'
+import { useHistory } from 'react-router'
+import ApiClient from '../../../utils/api-client'
+import RecordJSON from '../../../../backend/decorators/record-json.interface'
+import { NoticeMessage } from '../../../store/with-notice'
+import recordToFormData from '../record-to-form-data'
+import { appendForceRefresh } from './append-force-refresh'
 
-const api = new ApiClient();
+const api = new ApiClient()
+
+interface ResourceNew {
+  record: RecordJSON;
+  handleChange: (propertyOrRecord: string | RecordJSON, value?: any) => void;
+  handleSubmit: (event: any) => boolean;
+  loading: boolean;
+}
 
 const useResourceNew = (
   initialRecord: RecordJSON | undefined,
   resourceId: string,
-  onNotice: (notice: NoticeMessage) => void
-) => {
+  onNotice: (notice: NoticeMessage) => void,
+): ResourceNew => {
   const [record, setRecord] = useState<RecordJSON>({
     ...initialRecord,
     params: initialRecord?.params ?? {},
     errors: initialRecord?.errors ?? {},
     populated: initialRecord?.populated ?? {},
-  } as any);
-  const [loading, setLoading] = useState(false);
-  const history = useHistory();
+  } as any)
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
   const handleChange = (
     propertyOrRecord: RecordJSON | string,
-    value?: any
+    value?: any,
   ): void => {
     if (
-      typeof value === "undefined" &&
-      !(typeof propertyOrRecord === 'string') &&
-      (propertyOrRecord).params
+      typeof value === 'undefined'
+      && !(typeof propertyOrRecord === 'string')
+      && propertyOrRecord.params
     ) {
-      setRecord(propertyOrRecord);
+      setRecord(propertyOrRecord)
     } else {
       setRecord(prev => ({
         ...prev,
-        params: { ...prev.params, [propertyOrRecord as string]: value }
-      }));
+        params: { ...prev.params, [propertyOrRecord as string]: value },
+      }))
     }
-  };
+  }
 
   const handleSubmit = (event): boolean => {
-    const formData = recordToFormData(record);
-    setLoading(true);
+    const formData = recordToFormData(record)
+    setLoading(true)
     api
       .resourceAction({
         resourceId,
         actionName: 'new',
         data: formData,
-        headers: { "Content-Type": "multipart/form-data" }
+        headers: { 'Content-Type': 'multipart/form-data' },
       })
-      .then(response => {
+      .then((response) => {
         if (response.data.notice) {
-          onNotice(response.data.notice);
+          onNotice(response.data.notice)
         }
         if (response.data.redirectUrl) {
-          history.push(appendForceRefresh(response.data.redirectUrl));
+          history.push(appendForceRefresh(response.data.redirectUrl))
         } else {
-          setRecord(prev => ({ ...prev, errors: response.data.record.errors }));
-          setLoading(false);
+          setRecord(prev => ({ ...prev, errors: response.data.record.errors }))
+          setLoading(false)
         }
       })
       .catch(() => {
-        setLoading(false);
+        setLoading(false)
         onNotice({
           message:
-            "There was an error updating record, Check out console to see more information.",
-          type: "error"
-        });
-      });
-    event.preventDefault();
-    return false;
-  };
+            'There was an error updating record, Check out console to see more information.',
+          type: 'error',
+        })
+      })
+    event.preventDefault()
+    return false
+  }
 
-  return { record, handleChange, handleSubmit, loading };
-};
+  return { record, handleChange, handleSubmit, loading }
+}
 
-export default useResourceNew;
+export default useResourceNew
