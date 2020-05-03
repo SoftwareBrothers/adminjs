2bd2e80c1e7a5f9cb53d93182d659d6449e6d8af
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-05-03 14:38:53 +0200

    chore: change sequelize config to js

--- example-app/models/index.js
+++ example-app/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize')
 
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(`${__dirname}/../config/config.json`)[env]
+const config = require(`${__dirname}/../config/config.js`)[env]
 const db = {}
 
 let sequelize
