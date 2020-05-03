6ef6c2bd0a7220681c1b3c866738157956f22595
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-05-03 14:54:39 +0200

    fix db config

--- example-app/config/config.js
+++ example-app/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
     operatorsAliases: false,
-    username: process.env.POSTGRES_USER | 'postgres',
-    password: process.env.POSTGRES_PASSWORD | 'postgres',
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
     port: process.env.POSTGRES_PORT || 5432,
     database: process.env.POSTGRES_DATABASE || 'adminbro_e2e_development',
     host: process.env.POSTGRES_HOST || '127.0.0.1',
