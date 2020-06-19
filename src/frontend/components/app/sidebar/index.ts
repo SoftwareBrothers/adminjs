eab4a70fab94a87caea6e9ef365915bde085bfb3
JonaszJestem <jonaszwiacek@gmail.com>
2020-06-19 14:53:38 +0200

    feat: Add export SidebarResource as this could be useful also

--- src/frontend/components/app/sidebar/index.ts
+++ src/frontend/components/app/sidebar/index.ts
@@ -1,5 +1,6 @@
 import Sidebar from './sidebar'
 import SidebarParent from './sidebar-parent'
 import groupResources from './utils/group-resources'
+import SidebarResource from './sidebar-resource'
 
-export { SidebarParent, Sidebar, groupResources }
+export { SidebarParent, Sidebar, groupResources, SidebarResource }
