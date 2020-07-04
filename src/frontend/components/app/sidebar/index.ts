1c1b5a0eb616e03141526f1e7aa592c4d8cd4ff3
JonaszJestem <jonaszwiacek@gmail.com>
2020-07-04 10:24:05 +0200

    fix: Apply CR suggestions

--- src/frontend/components/app/sidebar/index.ts
+++ src/frontend/components/app/sidebar/index.ts
@@ -2,5 +2,6 @@ import Sidebar from './sidebar'
 import SidebarParent from './sidebar-parent'
 import groupResources from './utils/group-resources'
 import SidebarResource from './sidebar-resource'
+import { SidebarResourceSectionProps } from './sidebar-resource-section'
 
-export { SidebarParent, Sidebar, groupResources, SidebarResource }
+export { SidebarParent, Sidebar, groupResources, SidebarResource, SidebarResourceSectionProps }
--- src/frontend/components/app/sidebar/sidebar-resource-section.tsx
+++ src/frontend/components/app/sidebar/sidebar-resource-section.tsx
@@ -4,10 +4,16 @@ import SidebarParent from './sidebar-parent'
 import ResourceJSON from '../../../../backend/decorators/resource-json.interface'
 import allowOverride from '../../../hoc/allow-override'
 
-type SidebarResourceSectionProps = {
+export type SidebarResourceSectionProps = {
     resources: ResourceJSON[];
 }
 
+/**
+ * Groups resources by sections and renders the list in {@link Sidebar}
+ *
+ * @component
+ * @subcategory Molecules
+ */
 const SidebarResourceSection: FC<SidebarResourceSectionProps> = ({ resources }) => {
   const groupedResources = groupResources(resources)
 
