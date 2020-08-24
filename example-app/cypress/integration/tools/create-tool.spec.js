506d2d802f4237bd0c965f630614c2eeab4c0272
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-24 23:43:29 +0200

    ci: fix cypress tests

--- example-app/cypress/integration/tools/create-tool.spec.js
+++ example-app/cypress/integration/tools/create-tool.spec.js
@@ -12,8 +12,16 @@ context('resources/Tool/actions/new', () => {
   })
 
   it('creates new tool by hitting enter instead of clicking submit', () => {
-    cy.get('[data-testid="property-edit-name"]').type('My Name{enter}')
+    const name = 'My Name'
+    cy.get('[data-testid="property-edit-name"]').type(`${name}{enter}`)
 
     cy.location('pathname').should('eq', '/admin/resources/Tool')
+
+    cy.get('td[data-property-name="name"]').contains(name).parents('tr')
+      .find('[data-testid="actions-dropdown"]')
+      .trigger('mouseover')
+    cy.get('td[data-property-name="name"]').contains(name).parents('tr')
+      .find('[data-testid="action-delete"]')
+      .click()
   })
 })
