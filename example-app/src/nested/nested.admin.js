46dacb9c4485b6c5bb1f3173f33e3268e9727020
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-27 18:00:29 +0200

    fix: onChange can also update other fields

--- example-app/src/nested/nested.admin.js
+++ example-app/src/nested/nested.admin.js
@@ -1,8 +1,15 @@
+const { default: AdminBro } = require('admin-bro')
 const { Nested } = require('./nested.entity')
 
 /** @type {import('admin-bro').ResourceOptions} */
 const options = {
-
+  properties: {
+    valueTrigger: {
+      components: {
+        edit: AdminBro.bundle('./value-trigger.component.tsx'),
+      },
+    },
+  },
 }
 
 module.exports = {
