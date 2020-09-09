c68e88be4414ede929899c90b82c9ebadf908cb4
Wojciech Krysiak <wojciechkrysiak@Wojciechs-MacBook-Pro.local>
2020-09-09 22:13:27 +0200

    ci: fix error with sequelize on example app

--- example-app/models/index.js
+++ example-app/models/index.js
@@ -20,7 +20,7 @@ fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
   })
 
