3e8c540c0da8303248f18601ba6de4ac78106cc4
Wojciech Krysiak <wojciechkrysiak@Wojciechs-MacBook-Pro.local>
2020-09-23 21:46:58 +0200

    docs: remove PropertyInEdit from the docs
    fixes: #599

--- src/frontend/components/property-type/base-property-props.ts
+++ src/frontend/components/property-type/base-property-props.ts
@@ -136,56 +136,7 @@ export type ShowPropertyProps = {
 
 
 /**
- * On change callback - It takes either
- * one argument which is entire {@link RecordJSON} or 2 arguments - one
- * __property.name__ and the second one: __value__. Used by the __edit__ and __filter__ components.
- *
- * Lets take a look at an example of overriding edit component:
- * ```typescript
- * import React, { ReactNode } from 'react'
- * import { BasePropertyProps, PropertyInEdit, StyledInput } from 'admin-bro'
- *
- * export default class Edit extends React.Component<PropertyProps> {
- *   constructor(props) {
- *     super(props)
- *     this.handleInputChange = this.handleInputChange.bind(this)
- *   }
- *
- *   handleInputChange(event): void {
- *     const { onChange, property, record } = this.props
- *
- *     // Here is the interesting part:
- *     onChange(property.name, event.target.value)
- *
- *     // or you can pass an entire record. This is the same as above but gives you
- *     // much more flexibility
- *     const newRecord = { ...record }
- *     newRecord.params[property.name] = event.target.value
- *     onChange(newRecord)
- *   }
- *
- *   render(): ReactNode {
- *     const { property, record } = this.props
- *     const error = record.errors && record.errors[property.name]
- *     const value = (record.params && typeof record.params[property.name] !== 'undefined')
- *       ? record.params[property.name]
- *       : ''
- *     return (
- *       <PropertyInEdit property={property} error={error}>
- *         <StyledInput
- *           type="text"
- *           className="input"
- *           id={property.name}
- *           name={property.name}
- *           onChange={this.handleInputChange}
- *           value={value}
- *         />
- *       </PropertyInEdit>
- *     )
- *   }
- * }
- * ```
- *
+ * @load ./docs/on-property-change.doc.md
  * @memberof BasePropertyComponent
  * @alias OnPropertyChange
  */
