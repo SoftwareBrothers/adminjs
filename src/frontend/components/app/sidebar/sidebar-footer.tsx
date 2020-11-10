d8cad99c71d7f92c2446116a0c4ac4cbdb2da556
MK <matthijs@kadijk.com>
2020-11-10 21:14:48 +0100

    show branding in sidebar footer only if optiojn is set
    relates to issue #672 should be in one PR

--- src/frontend/components/app/sidebar/sidebar-footer.tsx
+++ src/frontend/components/app/sidebar/sidebar-footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Box, SoftwareBrothers } from '@admin-bro/design-system'
+import { useSelector } from 'react-redux'
 
 import allowOverride from '../../../hoc/allow-override'
+import { ReduxState } from '../../../store/store'
 
 const SidebarFooter: React.FC = () => (
+  const branding = useSelector((state: ReduxState) => state.branding)
   <Box mt="lg">
-    <SoftwareBrothers />
+    {branding?.softwareBrothers && <SoftwareBrothers />} 
   </Box>
 )
 
