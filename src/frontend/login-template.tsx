7b7416ec17676bebb1e0925d0a19448825853d5c
Kyle <me@proximity.sh>
2022-01-03 06:23:12 -0600

    fix: add branding.companyName to the login page's title (#954)
    Closes https://github.com/SoftwareBrothers/adminjs/issues/949

--- src/frontend/login-template.tsx
+++ src/frontend/login-template.tsx
@@ -87,7 +87,7 @@ const html = async (
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>AdminPanel</title>
+      <title>${branding.companyName}</title>
       ${style}
       ${faviconTag}
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" type="text/css">
