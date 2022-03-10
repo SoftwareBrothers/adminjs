e9dcdf5d06ecf00a8692e933de3002f5d7471992
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-10 09:24:42 +0100

    chore: fixed linting

--- src/frontend/utils/branding-provider.tsx
+++ src/frontend/utils/branding-provider.tsx
@@ -1,45 +1,42 @@
-import React, { createContext, FC, useContext } from 'react';
-import { useSelector } from 'react-redux';
-import { DefaultTheme, ThemeProvider } from 'styled-components';
-import { useLocalStorage } from '../hooks/use-local-storage';
-import { ReduxState } from '../store';
+import React, { createContext, FC, useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { DefaultTheme, ThemeProvider } from 'styled-components'
+import { useLocalStorage } from '../hooks/use-local-storage'
+import { ReduxState } from '../store'
 
 const useBrandingProviderProps = () => {
-  return useSelector<
-    ReduxState,
-    [ReduxState['branding'], ReduxState['availableBrandings']]
-  >(({ branding, availableBrandings }) => [branding, availableBrandings]);
-};
+  return useSelector<ReduxState, [ReduxState['branding'], ReduxState['availableBrandings']]>(
+    ({ branding, availableBrandings }) => [branding, availableBrandings]
+  )
+}
 
-type BrandingContextValue = ReturnType<typeof useBrandingProviderProps>;
+type BrandingContextValue = ReturnType<typeof useBrandingProviderProps>
 
-const BrandingContext = createContext<BrandingContextValue | undefined>(
-  undefined
-);
+const BrandingContext = createContext<BrandingContextValue | undefined>(undefined)
 
 interface BrandingProviderProps {
-  theme: DefaultTheme;
+  theme: DefaultTheme
 }
 
 const BrandingProvider: FC<BrandingProviderProps> = ({ children, theme }) => {
-  const value = useBrandingProviderProps();
-  const [storedTheme] = useLocalStorage<DefaultTheme>('adminjs-theme', theme);
+  const value = useBrandingProviderProps()
+  const [storedTheme] = useLocalStorage<DefaultTheme>('adminjs-theme', theme)
 
   return (
     <BrandingContext.Provider value={value}>
       <ThemeProvider theme={storedTheme || theme}>{children}</ThemeProvider>
     </BrandingContext.Provider>
-  );
-};
+  )
+}
 
 export const useBranding = (): BrandingContextValue => {
-  const ctx = useContext(BrandingContext);
+  const ctx = useContext(BrandingContext)
 
   if (!ctx) {
-    throw new Error('Component beyond BrandingContext!');
+    throw new Error('Component beyond BrandingContext!')
   }
 
-  return ctx;
-};
+  return ctx
+}
 
-export default BrandingProvider;
+export default BrandingProvider
