50b7e8d34799df3a34c0a3c050e8ccaa91d03fdc
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-28 09:44:31 +0200

    chore: removed adminjs demo package

--- packages/adminjs-demo/cypress/integration/example.spec.ts
+++
@@ -1,9 +0,0 @@
-context('Dashboard Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('shows overridden sidebar footer', () => {
-    cy.contains('Welcome on Board')
-  })
-})
--- packages/adminjs-demo/src/admin/resources/tag/tag-resource.ts
+++
@@ -1,8 +0,0 @@
-import { ResourceOptions, FeatureType } from 'adminjs'
-import { ContentParent } from '../../parents'
-
-export const features: Array<FeatureType> = []
-
-export const options: ResourceOptions = {
-  navigation: ContentParent,
-}
