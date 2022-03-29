b3917445cd115fb4e598fd095e814814ee76447e
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-29 09:29:42 +0200

    chore: pass options to rich text

--- src/frontend/components/property-type/richtext/edit.tsx
+++ src/frontend/components/property-type/richtext/edit.tsx
@@ -6,7 +6,7 @@ import { PropertyLabel } from '../utils/property-label'
 
 const Edit: FC<EditPropertyProps> = (props) => {
   const { property, record, onChange } = props
-  const value = record.params?.[property.path] || ''
+  const value = record.params?.[property.path] ?? ''
   const error = record.errors && record.errors[property.path]
 
   const handleUpdate = useCallback((newValue: string) => {
@@ -16,7 +16,7 @@ const Edit: FC<EditPropertyProps> = (props) => {
   return (
     <FormGroup error={Boolean(error)}>
       <PropertyLabel property={property} />
-      <RichTextEditor value={value} onChange={handleUpdate} />
+      <RichTextEditor value={value} onChange={handleUpdate} options={property.props} />
       <FormMessage>{error?.message}</FormMessage>
     </FormGroup>
   )
