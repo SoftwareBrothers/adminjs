043c626427b45b08290e9c2c493eccf2c77f9f5d
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-04-06 09:42:53 +0200

    feat: store filters in ls

--- src/frontend/hooks/use-history-listen.ts
+++ src/frontend/hooks/use-history-listen.ts
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router'
+import { useLocalStorage } from '../hooks/use-local-storage'
 import { ReduxState, RouterProps } from '../store'
-import { changeRoute } from '../store/actions/route-changed'
+import { changeRoute, initializeRoute } from '../store/actions/route-changed'
 
 const useHistoryListen = (): void => {
   const location = useLocation()
-  const { to = {} } = useSelector<ReduxState, RouterProps>(state => state.router)
+  const [storedPath, setStoredPath] = useLocalStorage<Partial<typeof location>>('prevPage', {})
+  const { to = {}, from = {} } = useSelector<ReduxState, RouterProps>(state => state.router)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (storedPath) {
+      dispatch(initializeRoute(storedPath))
+    }
+  }, [])
 
   useEffect(() => {
     const previousPath = [to.pathname, to.search].join('')
@@ -17,6 +25,12 @@ const useHistoryListen = (): void => {
       dispatch(changeRoute(location))
     }
   }, [location])
+
+  useEffect(() => {
+    if (from.pathname) {
+      setStoredPath(from)
+    }
+  }, [from])
 }
 
 export default useHistoryListen
