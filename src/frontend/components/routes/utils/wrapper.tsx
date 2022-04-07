9154937c63b71c1e28b3ed6508c13d73e4f7dd90
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2022-04-07 10:52:32 +0200

    fix: fix transparent bg in record actions

--- src/frontend/components/routes/utils/wrapper.tsx
+++ src/frontend/components/routes/utils/wrapper.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Box, BoxProps, DrawerContent, DrawerFooter } from '@adminjs/design-system'
 
-const StyledWrapper = styled(Box)`
+const StyledResourceActionWrapper = styled(Box)`
   & > ${DrawerContent} {
     background: ${({ theme }): string => theme.colors.white};
     padding: ${({ theme }): string => theme.space.xxl};
@@ -16,14 +16,28 @@ const StyledWrapper = styled(Box)`
   }
 `
 
-const Wrapper: React.FC<BoxProps> = (props) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { children, variant, color, ...rest } = props
+const StyledRecordActionWrapper = styled(Box)`
+  & ${DrawerContent} {
+    background: ${({ theme }): string => theme.colors.white};
+    padding: ${({ theme }): string => theme.space.xxl};
+    overflow: visible;
+  }
 
+  & ${DrawerFooter} {
+    background: ${({ theme }): string => theme.colors.white};
+    padding: 0 ${({ theme }): string => theme.space.xxl} ${({ theme }): string => theme.space.xxl};
+  }
+`
+
+const Wrapper: React.FC<BoxProps & { actionType?: string }> = (props) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { children, variant, color, actionType = 'record', ...rest } = props
+
+  const Component = actionType === 'record' ? StyledRecordActionWrapper : StyledResourceActionWrapper
   return (
-    <StyledWrapper {...rest} variant="grey" mx="auto">
+    <Component {...rest} variant="grey" mx="auto">
       {children}
-    </StyledWrapper>
+    </Component>
   )
 }
 
