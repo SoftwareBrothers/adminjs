ad76c007539b4eb9a0416db46b047d5bce0c0723
panda2134 <ljypanda@live.com>
2022-04-24 01:07:55 +0800

    locale: add Chinese (simplified) translation

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,11 +1,13 @@
 import enLocale from './en'
 import uaLocale from './ua'
+import zhCNLocale from './zh-CN'
 
 export * from './config'
 
 export const locales = {
   en: enLocale,
   ua: uaLocale,
+  'zh-cn': zhCNLocale
 }
 
-export { uaLocale, enLocale }
+export { uaLocale, enLocale, zhCNLocale }
