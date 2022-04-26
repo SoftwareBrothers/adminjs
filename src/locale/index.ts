387a4a989095438167b8204b3b88aa2a9fc4da78
panda2134 <me@panda2134.site>
2022-04-26 16:46:59 +0800

    feat(locale): add Chinese (simplified) translation (#1106)(@panda2134)
    * locale: add Chinese (simplified) translation
    
    * fix: typo with import
    
    * fix: lang code
    
    * fix: translation of actions.new
    
    * fix: filter translation
    
    * fix: eslint error
    
    * fix: typo in zh-CN translation

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,11 +1,13 @@
 import enLocale from './en'
 import uaLocale from './ua'
+import zhCNLocale from './zh-cn'
 
 export * from './config'
 
 export const locales = {
   en: enLocale,
   ua: uaLocale,
+  'zh-CN': zhCNLocale,
 }
 
-export { uaLocale, enLocale }
+export { uaLocale, enLocale, zhCNLocale }
