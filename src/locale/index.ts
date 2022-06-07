cd6e567e31a67433aaba5c27a338787301c0bd80
Isaac Pontes <isaaclfp@outlook.com>
2022-06-07 00:03:32 -0300

    feat(locale): add brazilian portuguese translations

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,6 +1,7 @@
 import enLocale from './en'
 import uaLocale from './ua'
 import zhCNLocale from './zh-cn'
+import ptBrLocale from './pt-br'
 
 export * from './config'
 
@@ -8,6 +9,7 @@ export const locales = {
   en: enLocale,
   ua: uaLocale,
   'zh-CN': zhCNLocale,
+  'pt-BR': ptBrLocale
 }
 
-export { uaLocale, enLocale, zhCNLocale }
+export { uaLocale, enLocale, zhCNLocale, ptBrLocale }
