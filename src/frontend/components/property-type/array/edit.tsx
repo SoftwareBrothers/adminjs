551916d862492cf8c93fb22ff43af99c89adc513
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2022-08-23 10:52:58 +0200

    fix: use @react-forked/dnd for drag and drop

--- src/frontend/components/property-type/array/edit.tsx
+++ src/frontend/components/property-type/array/edit.tsx
@@ -1,155 +1,70 @@
-import React, { MouseEvent, useCallback, useMemo, memo, useRef } from 'react'
+import React, { MouseEvent, useCallback } from 'react'
 import { Button, Section, FormGroup, FormMessage, Icon, Box } from '@adminjs/design-system'
-import { DropTargetMonitor, useDrag, useDrop } from 'react-dnd'
+import { DragDropContext, Droppable, Draggable, DropResult } from '@react-forked/dnd'
 
 import AddNewItemButton from './add-new-item-translation'
 import { flat } from '../../../../utils'
 import { EditPropertyPropsInArray } from '../base-property-props'
 import { PropertyLabel } from '../utils/property-label'
-import { convertToSubProperty, getIndexFromSubpropertyPath } from './convert-to-sub-property'
+import { convertToSubProperty } from './convert-to-sub-property'
 import { PropertyJSON } from '../../../interfaces'
 import { removeSubProperty } from './remove-sub-property'
 
-const DRAGGABLE_ELEMENT = 'ArrayItem'
-const VALUE_PATH_SEPARATOR = '~'
-
 type EditProps = Required<EditPropertyPropsInArray>
-
-type FindItemResult = {
-  item: any;
-  path: string;
-}
 
 type ItemRendererProps = {
   onDelete: (event: MouseEvent, property: PropertyJSON) => boolean;
-  moveItem: (id: string, destinationPath: string) => void;
-  findItem: (id: string) => FindItemResult;
+  index: number;
   isDraggable: boolean;
 }
 
-const encodeId = (value: string, path: string) => window.btoa(unescape(encodeURIComponent(`${value}${VALUE_PATH_SEPARATOR}${path}`)))
-const decodeId = (id: string) => {
-  const decoded = decodeURIComponent(escape(window.atob(id)))
-
-  const [value, path] = decoded.split(VALUE_PATH_SEPARATOR)
-
-  return [JSON.parse(value ?? '{}'), path]
-}
-
-const ItemRenderer: React.FC<EditProps & ItemRendererProps> = memo((props) => {
-  const ref = useRef<HTMLElement | null>(null)
-  const { ItemComponent, property, onDelete, record, findItem, moveItem, isDraggable } = props
-  const uniqueDraggableId = encodeId(
-    JSON.stringify(flat.get(record.params, property.path)),
-    property.path,
-  )
-
-  const originalPath = property.path
-  const [{ isDragging, handlerId }, connectDrag] = useDrag({
-    type: DRAGGABLE_ELEMENT,
-    item: { id: uniqueDraggableId, path: originalPath },
-    collect: (monitor) => {
-      const result = {
-        handlerId: monitor.getHandlerId(),
-        isDragging: monitor.isDragging(),
-      }
-      return result
-    },
-    end: (item, monitor) => {
-      const { id: droppedId, path } = item
-      if (!monitor.didDrop()) {
-        moveItem(droppedId, path)
-      } else {
-        moveItem(droppedId, monitor.getItem().path)
-      }
-    },
-  }, [moveItem, uniqueDraggableId, originalPath, record])
-
-  const [, connectDrop] = useDrop(
-    () => ({
-      accept: DRAGGABLE_ELEMENT,
-      hover(
-        _,
-        monitor: DropTargetMonitor<{ id: string; path: string}>,
-      ) {
-        const sourcePath = monitor.getItem().path
-        const sourceIndex = getIndexFromSubpropertyPath(sourcePath)
-        const destinationPath = originalPath
-        const destinationIndex = getIndexFromSubpropertyPath(destinationPath)
-
-        if (sourceIndex === destinationIndex || !ref.current) {
-          return
-        }
-
-        const hoverBoundingRect = ref.current.getBoundingClientRect()
-        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-        const clientOffset = monitor.getClientOffset()
-        if (!clientOffset) return
-
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top
-
-        // Dragging downwards
-        if (sourceIndex < destinationIndex && hoverClientY < hoverMiddleY) {
-          return
-        }
-
-        // Dragging upwards
-        if (sourceIndex > destinationIndex && hoverClientY > hoverMiddleY) {
-          return
-        }
-
-        monitor.getItem().path = destinationPath
-      },
-    }),
-    [findItem, moveItem, originalPath, uniqueDraggableId, record],
-  )
-
-  if (isDraggable) {
-    connectDrag(ref)
-    connectDrop(ref)
-  }
-
-  const opacity = isDragging ? 0.8 : 1
+const ItemRenderer: React.FC<EditProps & ItemRendererProps> = (props) => {
+  const { ItemComponent, property, onDelete, index, record, isDraggable } = props
+  const uniqueDraggableId = window.btoa(unescape(encodeURIComponent(`${JSON.stringify(flat.get(record.params, property.path))}-${property.path}`)))
 
   return (
-    <Box
-      ref={ref}
-      data-handler-id={handlerId}
-      backgroundColor="white"
-      flex
-      flexDirection="row"
-      alignItems="center"
-      style={{ opacity }}
-      data-testid={property.path}
+    <Draggable
+      draggableId={uniqueDraggableId}
+      index={index}
+      key={uniqueDraggableId}
+      isDragDisabled={!isDraggable}
     >
-      <Box flexGrow={1}>
-        <ItemComponent {...props} />
-      </Box>
-      <Box flexShrink={0} ml="lg">
-        <Button
-          rounded
-          ml="default"
-          data-testid="delete-item"
-          type="button"
-          size="icon"
-          onClick={(event): boolean => onDelete(event, property)}
-          variant="danger"
+      {(provided): JSX.Element => (
+        <Box
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          backgroundColor="white"
+          flex
+          flexDirection="row"
+          alignItems="center"
+          data-testid={property.path}
         >
-          <Icon icon="TrashCan" />
-        </Button>
-      </Box>
-    </Box>
+          <Box flexGrow={1}>
+            <ItemComponent {...props} />
+          </Box>
+          <Box flexShrink={0} ml="lg">
+            <Button
+              rounded
+              ml="default"
+              data-testid="delete-item"
+              type="button"
+              size="icon"
+              onClick={(event): boolean => onDelete(event, property)}
+              variant="danger"
+            >
+              <Icon icon="TrashCan" />
+            </Button>
+          </Box>
+        </Box>
+      )}
+    </Draggable>
   )
-})
+}
 
-const InputsInSection: React.FC<EditProps> = memo((props) => {
+const InputsInSection: React.FC<EditProps> = (props) => {
   const { property, record, resource, onChange } = props
-
-  const items = useMemo(() => flat.get(record.params, property.path) || [], [record.params])
-  const itemProperties = useMemo(
-    () => items.map((_, i) => convertToSubProperty(props.property, i)),
-    [items],
-  )
+  const items = flat.get(record.params, property.path) || []
 
   const addNew = useCallback((event: MouseEvent): boolean => {
     const newItems = [
@@ -168,56 +83,53 @@ const InputsInSection: React.FC<EditProps> = memo((props) => {
     return false
   }, [record, onChange, property])
 
-  const findItem = useCallback(
-    (id: string): FindItemResult => {
-      const [item, path] = decodeId(id)
-      return {
-        item,
-        path,
-      }
-    },
-    [items],
-  )
+  const handleOnDragEnd = useCallback((result: DropResult): void => {
+    const { source, destination } = result
 
-  const moveItem = useCallback(
-    (id: string, destinationPath: string) => {
-      const { path: sourcePath } = findItem(id)
-      const destinationIndex = getIndexFromSubpropertyPath(destinationPath)
-      const sourceIndex = getIndexFromSubpropertyPath(sourcePath)
+    if (!source || !destination || destination.index === source.index) return
 
-      const itemsCopy = items
-      const [sourceItem] = itemsCopy.splice(sourceIndex, 1)
-      itemsCopy.splice(destinationIndex, 0, sourceItem)
+    const itemsCopy = Array.from(items)
+    const [sourceItem] = itemsCopy.splice(source.index, 1)
+    itemsCopy.splice(destination.index, 0, sourceItem)
 
-      onChange(property.path, itemsCopy)
-    },
-    [findItem, items, onChange],
-  )
+    onChange(property.path, itemsCopy)
+  }, [record, onChange, property])
 
   return (
-    <Section
-      my="lg"
-      className={property.path}
-    >
-      {itemProperties.map((itemProperty: PropertyJSON) => (
-        <ItemRenderer
-          {...props}
-          key={itemProperty.path}
-          property={itemProperty}
-          isDraggable={property.isDraggable}
-          onDelete={removeItem}
-          moveItem={moveItem}
-          findItem={findItem}
-        />
-      ))}
-      <Button onClick={addNew} type="button" rounded>
-        <AddNewItemButton resource={resource} property={property} />
-      </Button>
-    </Section>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId={property.path}>
+        {(provided): JSX.Element => (
+          <Section
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            mt="xl"
+            className={property.path}
+          >
+            {items.map((item, i) => {
+              const itemProperty = convertToSubProperty(property, i)
+              return (
+                <ItemRenderer
+                  {...props}
+                  property={itemProperty}
+                  isDraggable={property.isDraggable}
+                  key={itemProperty.path}
+                  onDelete={removeItem}
+                  index={i}
+                />
+              )
+            })}
+            {provided.placeholder}
+            <Button onClick={addNew} type="button" rounded>
+              <AddNewItemButton resource={resource} property={property} />
+            </Button>
+          </Section>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
-})
+}
 
-const Edit: React.FC<EditProps> = memo((props) => {
+const Edit: React.FC<EditProps> = (props) => {
   const { property, record, testId } = props
   const error = record.errors && record.errors[property.propertyPath]
 
@@ -228,7 +140,7 @@ const Edit: React.FC<EditProps> = memo((props) => {
       <FormMessage>{error && error.message}</FormMessage>
     </FormGroup>
   )
-})
+}
 
 export {
   Edit as default,
