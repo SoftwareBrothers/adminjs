7e3e67167567f08296105c3ccd8aa5c058453a28
Jarosław Wasiak <jaroslaw.wasiak@rst.com.pl>
2022-10-06 08:19:49 +0200

    fix: boolean field in filter drawer

--- src/frontend/components/property-type/boolean/filter.tsx
+++ src/frontend/components/property-type/boolean/filter.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { FormGroup, Label, Select } from '@adminjs/design-system'
 
 import mapValue from './map-value'
 import { FilterPropertyProps } from '../base-property-props'
-import allowOverride from '../../../hoc/allow-override'
 
 const boolValue = (s: string): boolean => {
   if (/true/i.test(s)) {
@@ -12,32 +11,39 @@ const boolValue = (s: string): boolean => {
   return false
 }
 
-const Filter: React.FC<FilterPropertyProps> = (props) => {
-  const { property, filter = {}, onChange } = props
-  const value = typeof filter[property.path] === 'undefined' ? '' : boolValue(filter[property.path])
-  const options = [
-    { value: true, label: mapValue(true) },
-    { value: false, label: mapValue(false) },
-  ]
-  const selected = options.find((o) => o.value === value)
-
-  const handleChange = (s) => {
-    const newValue = s ? s.value : ''
-    onChange(property.path, newValue)
+class Filter extends React.PureComponent<FilterPropertyProps> {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  return (
-    <FormGroup>
-      <Label>{property.label}</Label>
-      <Select
-        variant="filter"
-        value={typeof selected === 'undefined' ? '' : selected}
-        isClearable
-        options={options}
-        onChange={handleChange}
-      />
-    </FormGroup>
-  )
+  handleChange(selected): void {
+    const { onChange, property } = this.props
+    const value = selected ? selected.value : ''
+    onChange(property.path, value)
+  }
+
+  render(): ReactNode {
+    const { property, filter = {} } = this.props
+    const value = typeof filter[property.path] === 'undefined' ? '' : boolValue(filter[property.path])
+    const options = [
+      { value: true, label: mapValue(true) },
+      { value: false, label: mapValue(false) },
+    ]
+    const selected = options.find((o) => o.value === value)
+    return (
+      <FormGroup>
+        <Label>{property.label}</Label>
+        <Select
+          variant="filter"
+          value={typeof selected === 'undefined' ? '' : selected}
+          isClearable
+          options={options}
+          onChange={this.handleChange}
+        />
+      </FormGroup>
+    )
+  }
 }
 
-export default allowOverride(Filter, 'DefaultBooleanFilterProperty')
+export default Filter
