ef6d8b76e2361003f83adcd57b7dc589e246cea8
yevhenii-hra <115156946+yevhenii-hra@users.noreply.github.com>
2022-11-04 13:36:06 +0200

    Update populator.ts
    (cherry picked from commit b48ea9a7796a4d797c5208825da951a708c7220b)

--- src/backend/utils/populator/populator.ts
+++ src/backend/utils/populator/populator.ts
@@ -8,6 +8,7 @@ import { populateProperty } from './populate-property'
  */
 export async function populator(
   records: Array<BaseRecord>,
+  excludePopulate: Array<string> = [],
 ): Promise<Array<BaseRecord>> {
   if (!records || !records.length) {
     return records
@@ -15,7 +16,12 @@ export async function populator(
   const resourceDecorator = records[0].resource.decorate()
   const allProperties = Object.values(resourceDecorator.getFlattenProperties())
 
-  const references = allProperties.filter((p) => !!p.reference())
+  const lowercaseExclude = excludePopulate.map((str) => str.toLowerCase())
+
+  const references = allProperties.filter(
+    (p) => !!p.reference()
+      && !lowercaseExclude.includes(p.name().toLowerCase()),
+  )
 
   await Promise.all(references.map(async (propertyDecorator) => {
     await populateProperty(records, propertyDecorator)
