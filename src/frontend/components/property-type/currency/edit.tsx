1001b3b576fb52bec229b3c0ae57239707ba36e4
Rafał Dzięgielewski <rafal.dzieg@gmail.com>
2023-01-10 11:07:54 +0100

    fix: revert textedit changes (#1375)
    * Revert "style: with buttons"
    
    This reverts commit c40c2a648411e0cfce7fc34f181f6a1f1606fdb9.
    
    * Revert "refactor: fix <TextEdit /> bugs"
    
    This reverts commit 6dc700e42f025580a2777eb71c231de70117198d.

--- src/frontend/components/property-type/currency/edit.tsx
+++ src/frontend/components/property-type/currency/edit.tsx
@@ -1,25 +1,24 @@
-import { CurrencyInput, FormGroup, FormMessage } from '@adminjs/design-system'
+import { CurrencyInputProps, FormGroup, FormMessage } from '@adminjs/design-system'
 import React, { FC, memo } from 'react'
 import { EditPropertyProps } from '../base-property-props'
 import { recordPropertyIsEqual } from '../record-property-is-equal'
 import { PropertyLabel } from '../utils/property-label'
 import allowOverride from '../../../hoc/allow-override'
+import { CurrencyInputWrapper } from './currency-input-wrapper'
 
 const Edit: FC<EditPropertyProps> = (props) => {
   const { onChange, property, record } = props
-  const propValue = record.params[property.path]
+  const propValue = record.params?.[property.path] ?? ''
   const error = record.errors?.[property.path]
 
   return (
     <FormGroup error={Boolean(error)}>
       <PropertyLabel property={property} />
-      <CurrencyInput
+      <CurrencyInputWrapper
         id={property.path}
-        name={property.path}
-        value={propValue ?? ''}
-        placeholder="<null>"
-        onValueChange={(value) => onChange(property.path, value)}
-        {...property.props}
+        initial={propValue}
+        options={property.props as CurrencyInputProps}
+        onChange={(value) => onChange(property.path, value)}
       />
       <FormMessage>{error && error.message}</FormMessage>
     </FormGroup>
