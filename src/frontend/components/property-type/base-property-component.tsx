6dff82fcd33c2daf7acda805bc0bce92e0d63518
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-01-10 10:17:41 +0100

    Revert "refactor: fix <TextEdit /> bugs"
    This reverts commit 6dc700e42f025580a2777eb71c231de70117198d.

--- src/frontend/components/property-type/base-property-component.tsx
+++ src/frontend/components/property-type/base-property-component.tsx
@@ -1,6 +1,6 @@
-import { Box, Label } from '@adminjs/design-system'
+import { Box } from '@adminjs/design-system'
 import { ReactComponentLike } from 'prop-types'
-import React, { useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 
 import ErrorBoundary from '../app/error-boundary'
 
@@ -68,8 +68,6 @@ const BasePropertyComponent: React.FC<BasePropertyComponentProps> = (props) => {
     // path either index (for array) or subProperty name.
     path: (baseProperty as PropertyJSON).path || baseProperty.propertyPath,
   }), [baseProperty])
-  const propValue = record?.params?.[property.path]
-  const [originalValue] = useState(propValue)
 
   const testId = `property-${where}-${property.path}`
   const contentTag = getActionElementCss(resource.id, where, property.path)
@@ -151,17 +149,6 @@ const BasePropertyComponent: React.FC<BasePropertyComponentProps> = (props) => {
           onChange={onChange}
           where={where}
         />
-        {propValue !== originalValue && (
-          <Label
-            variant="light"
-            inline
-            onClick={() => {
-              onChange!(property.path, originalValue)
-            }}
-          >
-            Reset
-          </Label>
-        )}
       </Box>
     </ErrorBoundary>
   )
