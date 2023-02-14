03af68370b53edcc7293109a6771730013b14f23
Rafał Dzięgielewski <rafal.dzieg@gmail.com>
2023-02-14 15:10:54 +0100

    fix: fix back button not closing drawer (#1416)

--- src/frontend/components/app/action-header/styled-back-button.tsx
+++ src/frontend/components/app/action-header/styled-back-button.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
-import { Link as RouterLink, useNavigate } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import {
   ButtonCSS,
   ButtonProps,
   Icon,
 } from '@adminjs/design-system'
+import { useSelector } from 'react-redux'
 
 import allowOverride from '../../../hoc/allow-override'
+import { DrawerProps, ReduxState, RouterProps } from '../../../store'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const StyledLink = styled(({ rounded, ...rest }) => <RouterLink {...rest} />)<ButtonProps>`${ButtonCSS}`
@@ -18,14 +20,26 @@ export type StyledBackButtonProps = {
 
 const StyledBackButton: React.FC<StyledBackButtonProps> = (props) => {
   const { showInDrawer } = props
+  const { previousRoute } = useSelector<ReduxState, DrawerProps>((state) => state.drawer)
+  const { from = {} } = useSelector<ReduxState, RouterProps>((state) => state.router)
   const cssCloseIcon = showInDrawer ? 'ChevronRight' : 'ChevronLeft'
 
-  const navigate = useNavigate()
+  const backLink = useMemo(() => {
+    if (!showInDrawer) {
+      return from?.pathname
+    }
+
+    if (previousRoute?.pathname) {
+      return previousRoute?.pathname
+    }
+
+    return from?.pathname
+  }, [previousRoute, from])
 
   return (
     <StyledLink
       size="icon"
-      to={navigate(-1)}
+      to={backLink}
       rounded
       mr="lg"
       type="button"
