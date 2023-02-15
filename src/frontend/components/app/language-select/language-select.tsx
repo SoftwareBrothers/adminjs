e8591fc61ec0fa0a6e22692cd000e2372d63f60c
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-02-15 10:03:28 +0100

    chore: imporved display language selector

--- src/frontend/components/app/language-select/language-select.tsx
+++ src/frontend/components/app/language-select/language-select.tsx
@@ -1,39 +1,44 @@
-import { Box, Button } from '@adminjs/design-system'
-import React, { FC } from 'react'
+import { Box, Button, DropDown, DropDownItem, DropDownMenu, DropDownTrigger, Icon } from '@adminjs/design-system'
+import React, { FC, useCallback } from 'react'
 import { useSelector } from 'react-redux'
-import { locales } from '../../../../locale'
+import { Locale, locales } from '../../../../locale'
 import { ReduxState } from '../../../store/store'
 import { useLocalStorage } from '../../../hooks'
 
-const LanguageButtons: FC = () => {
+const LanguageSelect: FC = () => {
   const { locale } = useSelector((state: ReduxState) => state)
-  const [loc, setLocale] = useLocalStorage('locale', locale)
+  const [storedLocale, setStoredLocale] = useLocalStorage('locale', locale)
   const { availableLanguages } = locale
-  const handleButton = (lng: string) => {
-    const selectedLocale = { ...locales[lng] }
-    selectedLocale.availableLanguages = availableLanguages
-    setLocale(selectedLocale)
+
+  const handleButton = useCallback((lng: string) => {
+    const selectedLocale: Locale = { ...locales[lng], availableLanguages }
+    setStoredLocale(selectedLocale)
     window.location.reload()
-  }
+  }, [])
+
   if (!availableLanguages.length) {
     return null
   }
+
   return (
-    <>
-      {availableLanguages.map(
-        (lang) => (
-          <Button key={lang} onClick={(): void => handleButton(lang)}>
-            {lang.substring(0, 2).toUpperCase()}
+    <Box flex alignItems="center">
+      <DropDown>
+        <DropDownTrigger>
+          <Button>
+            <Icon icon="Globe" />
+            {storedLocale.language}
           </Button>
-        ),
-      )}
-    </>
+        </DropDownTrigger>
+        <DropDownMenu>
+          {availableLanguages.map((lang) => (
+            <DropDownItem key={lang} onClick={() => handleButton(lang)}>
+              {lang}
+            </DropDownItem>
+          ))}
+        </DropDownMenu>
+      </DropDown>
+    </Box>
   )
 }
 
-const LanguageSelect: FC = () => (
-  <Box flex>
-    <LanguageButtons />
-  </Box>
-)
 export default LanguageSelect
