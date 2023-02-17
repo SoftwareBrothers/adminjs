90e11d2bce41b5961f9342baf1fb5454af05a216
Diogo de Azevedo Silva <DiogoAbu@users.noreply.github.com>
2023-02-17 14:22:39 -0300

    fix: translate boolean values on filter

--- src/frontend/components/property-type/boolean/filter.tsx
+++ src/frontend/components/property-type/boolean/filter.tsx
@@ -4,6 +4,7 @@ import { FormGroup, Label, Select } from '@adminjs/design-system'
 import mapValue from './map-value'
 import { FilterPropertyProps } from '../base-property-props'
 import allowOverride from '../../../hoc/allow-override'
+import { useTranslation } from '../../../hooks'
 
 const boolValue = (s: string): boolean => {
   if (/true/i.test(s)) {
@@ -13,11 +14,19 @@ const boolValue = (s: string): boolean => {
 }
 
 const Filter: React.FC<FilterPropertyProps> = (props) => {
-  const { property, filter = {}, onChange } = props
+  const { resource, property, filter = {}, onChange } = props
+  const { translateProperty } = useTranslation()
+
   const value = typeof filter[property.path] === 'undefined' ? '' : boolValue(filter[property.path])
   const options = [
-    { value: true, label: mapValue(true) },
-    { value: false, label: mapValue(false) },
+    {
+      value: true,
+      label: translateProperty(`${property.path}.true`, resource.id, { defaultValue: mapValue(true) }),
+    },
+    {
+      value: false,
+      label: translateProperty(`${property.path}.false`, resource.id, { defaultValue: mapValue(false) }),
+    },
   ]
   const selected = options.find((o) => o.value === value)
   const handleChange = (s) => {
