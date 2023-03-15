ba7c6f7f953e9797198d63edf5836b7969ce02dd
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-03-15 14:52:53 +0100

    chore: fix exports/imports

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,10 @@ import { ReduxState } from './types/src/frontend/store/store.js'
 export * from './types/src/index.js'
 
 export {
-  AdminJS as default,
+  AdminJS,
   ReduxState,
 }
+
+export default AdminJS
 
 declare const REDUX_STATE: ReduxState
--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import AdminJS from './lib/index.js'
 
 export * from './lib/index.js'
 
+export {
+  AdminJS,
+}
+
 export default AdminJS
