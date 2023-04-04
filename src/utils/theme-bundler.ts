965265fa7a81319690536adabfc6dfcf6ffc0bdd
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-04-04 10:50:33 +0200

    chore: fixed require themes resolve

--- src/utils/theme-bundler.ts
+++ src/utils/theme-bundler.ts
@@ -2,8 +2,8 @@ import { createRequire } from 'node:module'
 import path from 'path'
 
 const require = createRequire(import.meta.url)
-const adminjsThemesDir = path.parse(require.resolve('@adminjs/themes')).dir
+const getAdminjsThemesDir = () => path.parse(require.resolve('@adminjs/themes')).dir
 
-export const bundlePath = (theme: string): string => path.join(adminjsThemesDir, `themes/${theme}/theme.bundle.js`)
+export const bundlePath = (theme: string): string => path.join(getAdminjsThemesDir(), `themes/${theme}/theme.bundle.js`)
 
-export const stylePath = (theme: string): string => path.join(adminjsThemesDir, `themes/${theme}/style.css`)
+export const stylePath = (theme: string): string => path.join(getAdminjsThemesDir(), `themes/${theme}/style.css`)
