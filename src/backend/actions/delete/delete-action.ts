650f192169e753b283ae4cdf9d1bb11193bdd57a
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-07-04 09:31:43 +0200

    fix: make delete action perform delete only for POST method

--- src/backend/actions/delete/delete-action.ts
+++ src/backend/actions/delete/delete-action.ts
@@ -30,11 +30,19 @@ export const DeleteAction: Action<RecordActionResponse> = {
    */
   handler: async (request, _response, context) => {
     const { record, resource, currentAdmin, h } = context
+
     if (!request.params.recordId || !record) {
       throw new NotFoundError([
         'You have to pass "recordId" to Delete Action',
       ].join('\n'), 'Action#handler')
     }
+
+    if (request.method === 'get') {
+      return {
+        record: record.toJSON(context.currentAdmin),
+      }
+    }
+
     try {
       await resource.delete(request.params.recordId, context)
     } catch (error) {
@@ -51,6 +59,7 @@ export const DeleteAction: Action<RecordActionResponse> = {
       }
       throw error
     }
+
     return {
       record: record.toJSON(currentAdmin),
       redirectUrl: h.resourceUrl({ resourceId: resource._decorated?.id() || resource.id() }),
