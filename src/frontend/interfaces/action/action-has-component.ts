09ade25a8251696d04e296d9f6c8e65c8db7c3c3
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-07-04 09:46:16 +0200

    fix: fix 'no component' error when using a resource action with no component

--- src/frontend/interfaces/action/action-has-component.ts
+++ src/frontend/interfaces/action/action-has-component.ts
@@ -1,5 +1,9 @@
 import { ActionJSON } from './action-json.interface.js'
 
-export const actionHasComponent = (action: ActionJSON): boolean => (
+export const actionHasDisabledComponent = (action: ActionJSON): boolean => (
   typeof action.component !== 'undefined' && action.component === false
+)
+
+export const actionHasCustomComponent = (action: ActionJSON): boolean => (
+  typeof action.component === 'string'
 )
