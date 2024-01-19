4c3158792277cc6dfb6c661ef9a650ad65c65bb7
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2024-01-19 12:00:53 +0100

    fix: add missing translations for boolean/select fields

--- src/frontend/components/property-type/boolean/filter.tsx
+++ src/frontend/components/property-type/boolean/filter.tsx
@@ -5,6 +5,7 @@ import mapValue from './map-value.js'
 import { FilterPropertyProps } from '../base-property-props.js'
 import allowOverride from '../../../hoc/allow-override.js'
 import PropertyLabel from '../utils/property-label/property-label.js'
+import { useTranslation } from '../../../hooks/index.js'
 
 const boolValue = (s: string): boolean => {
   if (/true/i.test(s)) {
@@ -15,10 +16,11 @@ const boolValue = (s: string): boolean => {
 
 const Filter: React.FC<FilterPropertyProps> = (props) => {
   const { property, filter = {}, onChange } = props
+  const { tp } = useTranslation()
   const value = typeof filter[property.path] === 'undefined' ? '' : boolValue(filter[property.path])
   const options = [
-    { value: true, label: mapValue(true) },
-    { value: false, label: mapValue(false) },
+    { value: true, label: tp(`${property.path}.true`, property.resourceId, { defaultValue: mapValue(true) }) },
+    { value: false, label: tp(`${property.path}.false`, property.resourceId, { defaultValue: mapValue(false) }) },
   ]
   const selected = options.find((o) => o.value === value)
   const handleChange = (s) => {
