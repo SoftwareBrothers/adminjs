89db81c65ccb250b6f41c7d7ccd060744da5a925
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2024-03-07 10:39:45 +0100

    feat(small): update default dashboard

--- src/frontend/components/app/default-dashboard.tsx
+++ src/frontend/components/app/default-dashboard.tsx
@@ -3,44 +3,31 @@ import { Box, Button, H2, H5, Illustration, IllustrationProps, Text } from '@adm
 import { styled } from '@adminjs/design-system/styled-components'
 
 import { useTranslation } from '../../hooks/index.js'
+import RocketSVG from './utils/rocket-svg.js'
+import DiscordLogo from './utils/discord-logo-svg.js'
 
-const pageHeaderHeight = 284
+const pageHeaderHeight = 300
 const pageHeaderPaddingY = 74
 const pageHeaderPaddingX = 250
 
 export const DashboardHeader: React.FC = () => {
   const { translateMessage } = useTranslation()
   return (
-    <Box position="relative" overflow="hidden" data-css="default-dashboard">
+    <Box data-css="default-dashboard">
       <Box
-        position="absolute"
-        top={50}
-        left={-10}
-        opacity={[0.2, 0.4, 1]}
-        animate
-      >
-        <Illustration variant="Rocket" />
-      </Box>
-      <Box
-        position="absolute"
-        top={-70}
-        right={-15}
-        opacity={[0.2, 0.4, 1]}
-        animate
-      >
-        <Illustration variant="Moon" />
-      </Box>
-      <Box
-        bg="grey100"
+        position="relative"
+        overflow="hidden"
+        bg="white"
         height={pageHeaderHeight}
         py={pageHeaderPaddingY}
         px={['default', 'lg', pageHeaderPaddingX]}
       >
-        <Text textAlign="center" color="white">
-          <H2>{translateMessage('welcomeOnBoard_title')}</H2>
-          <Text opacity={0.8}>
-            {translateMessage('welcomeOnBoard_subtitle')}
-          </Text>
+        <Box position="absolute" top={30} left={0} opacity={0.9} animate display={['none', 'none', 'none', 'block']}>
+          <RocketSVG />
+        </Box>
+        <Text textAlign="center" color="grey100">
+          <H2 fontWeight="bold">{translateMessage('welcomeOnBoard_title')}</H2>
+          <Text opacity={0.8}>{translateMessage('welcomeOnBoard_subtitle')}</Text>
         </Text>
       </Box>
     </Box>
@@ -48,43 +35,50 @@ export const DashboardHeader: React.FC = () => {
 }
 
 type BoxType = {
-  variant: string;
-  title: string;
-  subtitle: string;
-  href: string;
+  variant: string
+  title: string
+  subtitle: string
+  href: string
 }
 
-const boxes = ({ translateMessage }): Array<BoxType> => [{
-  variant: 'Planet',
-  title: translateMessage('addingResources_title'),
-  subtitle: translateMessage('addingResources_subtitle'),
-  href: 'https://adminjs.co/tutorial-passing-resources.html',
-}, {
-  variant: 'DocumentCheck',
-  title: translateMessage('customizeResources_title'),
-  subtitle: translateMessage('customizeResources_subtitle'),
-  href: 'https://adminjs.co/tutorial-customizing-resources.html',
-}, {
-  variant: 'DocumentSearch',
-  title: translateMessage('customizeActions_title'),
-  subtitle: translateMessage('customizeActions_subtitle'),
-  href: 'https://adminjs.co/tutorial-actions.html',
-}, {
-  variant: 'FlagInCog',
-  title: translateMessage('writeOwnComponents_title'),
-  subtitle: translateMessage('writeOwnComponents_subtitle'),
-  href: 'https://adminjs.co/tutorial-writing-react-components.html',
-}, {
-  variant: 'Folders',
-  title: translateMessage('customDashboard_title'),
-  subtitle: translateMessage('customDashboard_subtitle'),
-  href: 'https://adminjs.co/tutorial-custom-dashboard.html',
-}, {
-  variant: 'Astronaut',
-  title: translateMessage('roleBasedAccess_title'),
-  subtitle: translateMessage('roleBasedAccess_subtitle'),
-  href: 'https://adminjs.co/tutorial-rbac.html',
-}]
+const boxes = ({ translateMessage }): Array<BoxType> => [
+  {
+    variant: 'Details',
+    title: translateMessage('addingResources_title'),
+    subtitle: translateMessage('addingResources_subtitle'),
+    href: 'https://docs.adminjs.co/basics/resource#providing-resources-explicitly',
+  },
+  {
+    variant: 'Docs',
+    title: translateMessage('customizeResources_title'),
+    subtitle: translateMessage('customizeResources_subtitle'),
+    href: 'https://docs.adminjs.co/basics/resource#customizing-resources',
+  },
+  {
+    variant: 'Plug',
+    title: translateMessage('customizeActions_title'),
+    subtitle: translateMessage('customizeActions_subtitle'),
+    href: 'https://docs.adminjs.co/basics/action',
+  },
+  {
+    variant: 'Cup',
+    title: translateMessage('writeOwnComponents_title'),
+    subtitle: translateMessage('writeOwnComponents_subtitle'),
+    href: 'https://docs.adminjs.co/ui-customization/writing-your-own-components',
+  },
+  {
+    variant: 'Photos',
+    title: translateMessage('customDashboard_title'),
+    subtitle: translateMessage('customDashboard_subtitle'),
+    href: 'https://docs.adminjs.co/ui-customization/dashboard-customization',
+  },
+  {
+    variant: 'IdentityCard',
+    title: translateMessage('roleBasedAccess_title'),
+    subtitle: translateMessage('roleBasedAccess_subtitle'),
+    href: 'https://docs.adminjs.co/tutorials/adding-role-based-access-control',
+  },
+]
 
 const Card = styled(Box)`
   display: ${({ flex }): string => (flex ? 'flex' : 'block')};
@@ -94,9 +88,15 @@ const Card = styled(Box)`
   border: 1px solid transparent;
   border-radius: ${({ theme }) => theme.space.md};
   transition: all 0.1s ease-in;
+
   &:hover {
-    border: 1px solid ${({ theme }) => theme.colors.primary100};
+    border: 1px solid ${({ theme }) => theme.colors.primary60};
     box-shadow: ${({ theme }) => theme.shadows.cardHover};
+  }
+
+  & .dsc-icon svg, .gh-icon svg {
+    width: 64px;
+    height: 64px;
   }
 `
 
@@ -132,15 +132,29 @@ export const Dashboard: React.FC = () => {
                   width={100}
                   height={70}
                 />
-                <H5 mt="lg">{box.title}</H5>
+                <H5 mt="md">{box.title}</H5>
                 <Text>{box.subtitle}</Text>
               </Text>
             </Card>
           </Box>
         ))}
+        <Card width={1} m="lg">
+          <Text textAlign="center">
+            <Illustration variant="AdminJSLogo" />
+            <H5>{translateMessage('needMoreSolutions_title')}</H5>
+            <Text>{translateMessage('needMoreSolutions_subtitle')}</Text>
+            <Text mt="xxl">
+              <Button as="a" variant="contained" href="https://forms.adminjs.co/" target="_blank">
+                {translateButton('contactUs')}
+              </Button>
+            </Text>
+          </Text>
+        </Card>
         <Box width={[1, 1, 1 / 2]} p="lg">
-          <Card as="a" flex href="https://adminjs.page.link/slack" target="_blank">
-            <Box flexShrink={0}><Illustration variant="SlackLogo" /></Box>
+          <Card as="a" flex href="https://adminjs.page.link/discord" target="_blank">
+            <Box flexShrink={0} className="dsc-icon">
+              <DiscordLogo />
+            </Box>
             <Box ml="xl">
               <H5>{translateMessage('community_title')}</H5>
               <Text>{translateMessage('community_subtitle')}</Text>
@@ -148,31 +162,21 @@ export const Dashboard: React.FC = () => {
           </Card>
         </Box>
         <Box width={[1, 1, 1 / 2]} p="lg">
-          <Card as="a" flex href="https://github.com/SoftwareBrothers/adminjs/issues" target="_blank">
-            <Box flexShrink={0}><Illustration variant="GithubLogo" /></Box>
+          <Card
+            as="a"
+            flex
+            href="https://github.com/SoftwareBrothers/adminjs/issues"
+            target="_blank"
+          >
+            <Box flexShrink={0} className="gh-icon">
+              <Illustration variant="GithubLogo" />
+            </Box>
             <Box ml="xl">
               <H5>{translateMessage('foundBug_title')}</H5>
               <Text>{translateMessage('foundBug_subtitle')}</Text>
             </Box>
           </Card>
         </Box>
-        <Card width={1} m="lg">
-          <Text textAlign="center">
-            <Illustration variant="AdminJSLogo" />
-            <H5>{translateMessage('needMoreSolutions_title')}</H5>
-            <Text>{translateMessage('needMoreSolutions_subtitle')}</Text>
-            <Text mt="xxl">
-              <Button
-                as="a"
-                variant="contained"
-                href="https://forms.adminjs.co/"
-                target="_blank"
-              >
-                {translateButton('contactUs')}
-              </Button>
-            </Text>
-          </Text>
-        </Card>
       </Box>
     </Box>
   )
