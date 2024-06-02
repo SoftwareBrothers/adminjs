838ebd492bd6b26193f914533df026e7d8ba4904
DestyK <nikita.karpov.1910@mail.ru>
2024-06-02 08:16:16 +0800

    fix: incorrect file path in Windows platform

--- src/utils/file-resolver.ts
+++ src/utils/file-resolver.ts
@@ -16,5 +16,24 @@ export const relativeFilePathResolver = (filePath: string, syntax: RegExp): stri
     throw new Error('STACK does not have a file url. Check out if the node version >= 8')
   }
   const executionPath = (pathNode8 && pathNode8[1]) || (pathNode10 && pathNode10[1])
-  return path.join(path.dirname(executionPath as string), filePath).replace(/^file:/gi, '')
+
+  /**
+  * decodeURIComponent used to decode parts of a path.
+  *
+  * Before: \D:\%D0%9D%D0%B8%D0%BA%D0%B8%D1%82%D0%B0\project\dist\components\photo
+  * After: \D:\Никита\project\dist\components\photo
+  */
+  const resultPath = path.join(path.dirname(executionPath), filePath).replace(/^file:/gi, '')
+  
+  /**
+  * If the separator is a backslash, remove the first one.
+  * If you do not do this, the file will not be found
+  * 
+  * Required for Windows
+  */
+  if (path.sep === '\\' && resultPath[0] === '\\') {
+    return resultPath.slice(1)
+  }
+  
+  return resultPath
 }
