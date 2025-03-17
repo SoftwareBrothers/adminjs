c4650b356c7c07d34886c3e48221514d3ac4a775
roggervalf <rogger.valvrd@gmail.com>
2025-03-17 00:47:26 -0600

    feat: support or operator

--- src/frontend/components/property-type/default-type/filter.tsx
+++ src/frontend/components/property-type/default-type/filter.tsx
@@ -7,22 +7,55 @@ import PropertyLabel from '../utils/property-label/property-label.js'
 import { useTranslation } from '../../../hooks/use-translation.js'
 import * as BackendFilter from '../../../../backend/utils/filter/filter.js'
 
-const { PARAM_SEPARATOR } = BackendFilter
+const { MATCHING_PATTERNS, OPERATOR_SEPARATOR, OPERATORS, PARAM_SEPARATOR } = BackendFilter
+
+const getKey = (path:string, patternMatching: string, operator:string) => {
+  if (patternMatching === MATCHING_PATTERNS.CO) {
+    if (operator === OPERATORS.AND) {
+      return path
+    }
+
+    return `${path}${PARAM_SEPARATOR}${operator}`
+  }
+  if (operator === OPERATORS.AND) {
+    return `${path}${PARAM_SEPARATOR}${patternMatching}`
+  }
+  return `${path}${PARAM_SEPARATOR}${operator}${OPERATOR_SEPARATOR}${patternMatching}`
+}
+
+const generatePossibleKeys = (path:string) => {
+  const possibleKeys:string[] = []
+
+  for (const matchingPattern of Object.values(MATCHING_PATTERNS)) {
+    for (const operator of Object.values(OPERATORS)) {
+      possibleKeys.push(getKey(path, matchingPattern, operator))
+    }
+  }
+
+  return possibleKeys
+}
 
 const Filter: React.FC<FilterPropertyProps> = (props) => {
   const { property, onChange, filter } = props
 
-  const possibleKeys = [
-    property.path,
-    `${property.path}${PARAM_SEPARATOR}equals`,
-    `${property.path}${PARAM_SEPARATOR}startsWith`,
-    `${property.path}${PARAM_SEPARATOR}endsWith`,
-  ]
+  const possibleKeys = generatePossibleKeys(property.path)
 
   const [currentKey, currentInput] = Object.entries(filter).find(
     ([key]) => possibleKeys.includes(key),
   ) || []
-  const currentOperator = (currentKey ? currentKey.split(PARAM_SEPARATOR)[1] : 'contains') || 'contains'
+  let currentPatternMatching = MATCHING_PATTERNS.CO
+  let currentOperator = OPERATORS.AND
+
+  if (currentKey) {
+    const filterProperties = currentKey.split(PARAM_SEPARATOR)
+    const param = (filterProperties[1] || '').split(OPERATOR_SEPARATOR)
+    const tentativeOperator = param[0] || ''
+    const tentativeMatchingPattern = (param.length > 1 ? param[1] : param[0]) || ''
+    currentOperator = Object.values(OPERATORS).includes(tentativeOperator)
+      ? tentativeOperator : OPERATORS.AND
+    currentPatternMatching = Object.values(MATCHING_PATTERNS).includes(tentativeMatchingPattern)
+      ? tentativeMatchingPattern : MATCHING_PATTERNS.CO
+  }
 
   const { tl } = useTranslation()
 
@@ -31,12 +64,8 @@ const Filter: React.FC<FilterPropertyProps> = (props) => {
   }
 
   const handleInputInComboChange = (event) => {
-    if (currentOperator === 'contains') {
-      onChange(property.path, event.target.value)
-    } else {
-      const key = `${property.path}${PARAM_SEPARATOR}${currentOperator}`
-      onChange(key, event.target.value)
-    }
+    const key = getKey(property.path, currentPatternMatching, currentOperator)
+    onChange(key, event.target.value)
   }
 
   const handleSelectChange = (selected) => {
@@ -44,8 +73,27 @@ const Filter: React.FC<FilterPropertyProps> = (props) => {
     onChange(property.path, value)
   }
 
-  const handleSelectInComboChange = (selected) => {
-    const changedKey = ((selected?.value || 'contains') !== 'contains') ? `${property.path}${PARAM_SEPARATOR}${selected.value}` : property.path
+  const handleSelectPatternMatchingInComboChange = (selected) => {
+    const changedKey = getKey(
+      property.path,
+      selected?.value || MATCHING_PATTERNS.CO,
+      currentOperator,
+    )
+
+    possibleKeys.forEach((key) => {
+      if (key !== changedKey) {
+        delete filter[key]
+      }
+    })
+    onChange(changedKey, currentInput || '')
+  }
+
+  const handleSelectOperatorInComboChange = (selected) => {
+    const changedKey = getKey(
+      property.path,
+      currentPatternMatching,
+      selected?.value || OPERATORS.AND,
+    )
 
     possibleKeys.forEach((key) => {
       if (key !== changedKey) {
@@ -56,7 +104,7 @@ const Filter: React.FC<FilterPropertyProps> = (props) => {
   }
 
   const renderInput = () => {
-    const valueKey = currentOperator === 'contains' ? property.path : `${property.path}${PARAM_SEPARATOR}${currentOperator}`
+    const valueKey = getKey(property.path, currentPatternMatching, currentOperator)
 
     const filterKey = `filter-${valueKey}`
     const value = filter[valueKey] || ''
@@ -80,39 +128,60 @@ const Filter: React.FC<FilterPropertyProps> = (props) => {
     }
 
     if (property.type === 'string') {
+      const patternMatching = { label: currentPatternMatching, value: currentPatternMatching }
       const operator = { label: currentOperator, value: currentOperator }
       return (
-        <Box flex flexDirection="row">
-          <Box flexGrow={0}>
-            <Input
-              name={filterKey}
-              onChange={handleInputInComboChange}
-              value={filter[currentKey || property.path] || ''}
-            />
-          </Box>
-          <Box flexGrow={1}>
-            <Select
-              value={operator}
-              options={[
-                {
-                  label: 'contains',
-                  value: 'contains',
-                },
-                {
-                  label: 'equals',
-                  value: 'equals',
-                },
-                {
-                  label: 'startsWith',
-                  value: 'startsWith',
-                },
-                {
-                  label: 'endsWith',
-                  value: 'endsWith',
-                },
-              ]}
-              onChange={handleSelectInComboChange}
-            />
+        <Box flex flexDirection="column">
+          <Input
+            name={filterKey}
+            onChange={handleInputInComboChange}
+            value={filter[currentKey || property.path] || ''}
+          />
+          <Box flex flexDirection="row">
+            <Box flexGrow={1}>
+              <Select
+                value={patternMatching}
+                options={[
+                  {
+                    label: MATCHING_PATTERNS.CO,
+                    value: MATCHING_PATTERNS.CO,
+                  },
+                  {
+                    label: MATCHING_PATTERNS.EQ,
+                    value: MATCHING_PATTERNS.EQ,
+                  },
+                  {
+                    label: MATCHING_PATTERNS.NE,
+                    value: MATCHING_PATTERNS.NE,
+                  },
+                  {
+                    label: MATCHING_PATTERNS.SW,
+                    value: MATCHING_PATTERNS.SW,
+                  },
+                  {
+                    label: MATCHING_PATTERNS.EW,
+                    value: MATCHING_PATTERNS.EW,
+                  },
+                ]}
+                onChange={handleSelectPatternMatchingInComboChange}
+              />
+            </Box>
+            <Box flexGrow={1}>
+              <Select
+                value={operator}
+                options={[
+                  {
+                    label: OPERATORS.AND,
+                    value: OPERATORS.AND,
+                  },
+                  {
+                    label: OPERATORS.OR,
+                    value: OPERATORS.OR,
+                  },
+                ]}
+                onChange={handleSelectOperatorInComboChange}
+              />
+            </Box>
           </Box>
         </Box>
       )
